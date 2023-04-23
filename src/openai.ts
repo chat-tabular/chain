5056993ce2dac74131dec054eaa5f320f04589ed
allen huang <alhuang@marinsoftware.com>
2023-04-23 09:28:46 +0800

    fix the vaule type

--- src/openai.ts
+++ src/openai.ts
@@ -16,7 +16,7 @@ You should not to explain, just show the action number and name.
 My Question: {QUESTION}
 `;
 
-export const TABLE_PROMPT = `You are working with javascript program, I will give you a \`table: {column: string[], rows: Row[]}\`, where Rows is {[column key]: value}.
+export const TABLE_PROMPT = `You are working with javascript program, I will give you a \`table: {column: string[], rows: Row[]}\`, where Rows is {[column key]: string}, notice the row value type is string.
 I will ask you question, you should return a javascript function to resolve my question.
 - function name should be \`${exportedFuncName}\`
 - input parameter as \`table\`, output should also return a table type
@@ -30,7 +30,7 @@ My table head rows are """
 
 Question: {QUESTION} `
 
-export const CHART_PROMPT = `You are working with javascript program to show me the chart by google chart library,  I will give you a \`table: {column: string[], rows: Row[]}\`, where Rows is {[column key]: value}.
+export const CHART_PROMPT = `You are working with javascript program to show me the chart by google chart library,  I will give you a \`table: {column: string[], rows: Row[]}\`, where Rows is {[column key]: value}, notice the row value type is string.
 I will ask you question, you should return a javascript function to show chart, powered by google chart libarary
 - chart container DOM element id is {CHART_CONTAINER_ID}
 - function name should be \`${exportedFuncName}\`
